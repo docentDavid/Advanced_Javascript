d299e8f6050d0910249a5c5b2f586eee2a0ca87f
David Schol <d.schol@fontys.nl>
2023-03-26 12:01:00 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("root").innerHTML = `
+<div>
+  <h1>Hello Vanilla!</h1>
+  <p>
+    We use the same configuration as Parcel to bundle this sandbox, you can find more
+    info about Parcel 
+    <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  </p>
+</div>
+`;
